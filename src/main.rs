1be651c57bbf7f777fbe3249b7623aac7602b3b3
Sl1mj1m14 <99777514+Sl1mj1m14@users.noreply.github.com>
2025-01-02 13:13:15 -0500

    Created empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-pub fn main () {
-    println!("hello world");
+use eframe::egui;
+
+fn main () -> eframe::Result {
+
+    let app = App {};
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+        .with_inner_size([400.0,400.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "temp",
+         options, 
+         Box::new(|_|Ok(Box::new(app)))
+    )
+
 }
+
+#[derive(Default)]
+struct App {}
+
+impl eframe::App for App {
+    fn update (&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+
+    }
+}
+
