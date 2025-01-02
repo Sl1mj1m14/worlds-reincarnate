c7b1bee807216979af2e0ce805ec59627b2b7104
Sl1mj1m14 <99777514+Sl1mj1m14@users.noreply.github.com>
2025-01-02 13:24:35 -0500

    Moved UI to module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
 use eframe::egui;
 
+mod ui;
+
+const APP_NAME: &str = "Worlds Reincarnate";
+const BASE_WIDTH: f32 = 400.0;
+const BASE_HEIGHT: f32 = 400.0;
+
 fn main () -> eframe::Result {
 
-    let app = App {};
+    let app = ui::App {};
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-        .with_inner_size([400.0,400.0]),
+        .with_inner_size([BASE_WIDTH, BASE_HEIGHT]),
         ..Default::default()
     };
 
     eframe::run_native(
-        "temp",
+        APP_NAME,
          options, 
          Box::new(|_|Ok(Box::new(app)))
     )
-
 }
 
-#[derive(Default)]
-struct App {}
 
-impl eframe::App for App {
-    fn update (&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
-
-    }
-}
 
---
+++ src/ui/mod.rs
@@ -0,0 +1,8 @@
+#[derive(Default)]
+pub struct App {}
+
+impl eframe::App for App {
+    fn update (&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+
+    }
+}
