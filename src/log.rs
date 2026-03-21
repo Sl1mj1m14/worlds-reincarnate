28e132d99fe9cddc0c1115ac475efa88e4b3bd9f
Sl1mj1m14 <99777514+Sl1mj1m14@users.noreply.github.com>
2026-03-21 18:36:09 -0500

    Implemented proper logging

--- src/log.rs
+++ src/log.rs
@@ -1,31 +1,90 @@
-//use crate::SESSION
+use std::{fmt::Error, fs::{File, OpenOptions}, path::PathBuf, sync::OnceLock, io::Write};
+
+use chrono::Local;
+use colored::{ColoredString, Colorize};
+
+use crate::{DEFAULT_TIMESTAMP, TIMESTAMP};
 
 const LOG_DIR: &str = "logs";
+const LOG_DEFAULT: &str = "log.txt";
 
-use std::{io::Error, path::{Path, PathBuf}};
+static LOG_NAME: OnceLock<String> = OnceLock::new();
 
-pub fn log (msg: String) {
-    //Handle errors
-    //Write to log file
-    println!("{msg}");
+const LIGHT_GREY: &[u8] = &[107; 3];
+
+pub trait Msg {
+    fn to_msg (&self) -> String;
 }
 
-pub fn close (session: u16, timestamp: String) -> Result<(),Error> {
+impl Msg for String {
+    fn to_msg (&self) -> String {
+        self.to_string()
+    }
+}
 
-    let mut log_name: String = String::from("log");
-    if session != 0 {log_name += &session.to_string()}
-    log_name += ".txt";
-    let path: PathBuf = [LOG_DIR,&log_name].iter().collect();
-    println!("Original Path is: {}",path.display());
-    if !path.exists() {return Ok(())}
+impl Msg for &str {
+    fn to_msg (&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Msg for Error {
+    fn to_msg (&self) -> String {
+        self.to_string()
+    }
+}
+
+pub fn start () {
+    let binding = DEFAULT_TIMESTAMP.to_string();
+    let timestamp = TIMESTAMP.get().unwrap_or(&binding);
+
+    let base: &str = "log_";
+    let ext: &str = ".txt";
+
+    let mut name: String = format!("{}{}{}",base,timestamp,ext);
+
+    let mut path: PathBuf = [LOG_DIR,&name].iter().collect();
+
+    let mut ver: i16 = 0;
+
+    while path.exists() {
+        ver += 1;
+        name = format!("{}{}-{}{}",base,timestamp,ver,ext);
+        path = [LOG_DIR,&name].iter().collect();
+    }
+
+    LOG_NAME.set(name).unwrap();
     
-    let mut append: i32 = 0;
+    let _ = File::create(path);
+}
 
-    let mut rename: String = String::from("log_");
-    rename += &timestamp;
-    let mut rename_path: PathBuf = [LOG_DIR,&rename,".txt"].iter().collect();
+pub fn log (code: u8, input: impl Msg) {
+    let mut msg: String = Msg::to_msg(&input);
+    let timestamp: String = Local::now().format("%Y-%m-%d %H:%M:%S : ").to_string();
 
-    Ok(())
+    let path: PathBuf = [LOG_DIR,LOG_NAME.get().unwrap_or(&LOG_DEFAULT.to_string())].iter().collect();
 
+    let log_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path);
+
+    let _ = match log_file {
+        Ok(mut file) => writeln!(file,"{}{}",timestamp,msg),
+        Err(_) => Ok(())
+    };
+
+    let msg_colored: ColoredString = match code {
+        1 => {
+            msg = String::from("WARNING: ") + &msg;
+            msg.yellow()
+        },
+        2 => {
+            msg = String::from("ERROR: ") + &msg;
+            msg.red()
+        },
+        _ => msg.white()
+    };
+    println!("{}{}",timestamp.truecolor(LIGHT_GREY[0],LIGHT_GREY[1],LIGHT_GREY[2]),msg_colored);
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-//use mc_classic;
-//use mc_classic_js;
+use std::sync::OnceLock;
 
-use std::{fs::{File, create_dir_all, read_dir, remove_dir_all, remove_file}, io::{Error, Read, Write}, mem, path::PathBuf};
-use regex::{self, Regex};
 use chrono::prelude::*;
 
-mod read;
+//mod read;
 mod write;
 //mod convert;
 
@@ -13,37 +10,16 @@ mod functions;
 mod world;
 mod log;
 
-const SESSION_DIR: &str = "session";
+static TIMESTAMP: OnceLock<String> = OnceLock::new();
+const DEFAULT_TIMESTAMP: &str = "19700101120000";
 
-static mut SESSION: u16 = 0;
+fn main () {
 
-fn main () -> Result<(),Error> {
-    println!("hello rewrite");
-
-    /***Opening Session File***/
-
-    let mut session: u16 = 0;
-
-    match clean_sessions() {
-        Ok(val) => session = val,
-        Err(err) => eprintln!("Session Error: {err}")
-    }
-
-    unsafe {SESSION = session}
-    
-    let mut lock_name: String = String::from("session");
-    if session != 0 {lock_name += &session.to_string()}
-    lock_name += ".lock";
-
-    let session_path: PathBuf = [SESSION_DIR,&lock_name].iter().collect();
     let timestamp: String = Local::now().format("%Y%m%d%H%M%S").to_string();
+    TIMESTAMP.set(timestamp).unwrap();
 
-    let mut session_lock = File::create(session_path.clone())?;
-    session_lock.write_all(timestamp.as_bytes())?;
-    session_lock = File::open(session_path)?;
-
-    println!("Session Started");
-
+    log::start();
+    log::log(0,format!("Session Started at {}",Local::now().format("%Y-%m-%d %H:%M:%S")));
 
 /*
     let level = read::read_classic_file(PathBuf::from("input/level.dat"));
@@ -62,61 +38,9 @@ fn main () -> Result<(),Error> {
 
     */
     //mem::drop(session_lock);
-    let _ = clean_sessions();
-    Ok(())
 }
 
-fn clean_sessions () -> Result<u16,Error> {
-    let mut session: u16 = 0;
-    let mut open: Vec<u16> = Vec::new();
 
-    create_dir_all(SESSION_DIR)?;
-
-    let pattern = match Regex::new(r"^session(?<num>\d*)\.lock$") {
-        Ok(val) => val,
-        Err(err) => {
-            //This error should never happen, but if it does only log files will break
-            eprintln!("Regex formatting error: {err}");
-            return Ok(0)
-        }
-    };
-
-    for entry in read_dir(SESSION_DIR)? {
-        let path = entry?.path();
-        if path.is_file() {
-            let file_name = path.file_name().unwrap_or_default().to_str().unwrap_or_default();
-            if pattern.is_match(file_name) {
-                println!("{}",path.display());
-                let id: u16 = match pattern.captures(file_name).unwrap()["num"].parse() {
-                    Ok(val) => val,
-                    Err(_) => 0
-                };
-                match File::open(path.clone()) {
-                    Ok(mut file) => {
-                        let mut timestamp: String = String::default();
-                        file.read_to_string(&mut timestamp).unwrap_or_default();
-                        let _ = log::close(id,timestamp);
-                    },
-                    Err(_) => {
-                        open.push(id);
-                        while open.contains(&session) {session += 1}
-                        println!("It is read as open...");
-                    }
-                }
-            }
-            let _ = remove_file(path);
-        } else {
-            match remove_dir_all(path) {
-                Ok(()) => (),
-                Err(err) => {
-                    println!("Broken directory inside session folder");
-                    eprintln!("Error: {err}");
-                }
-            }
-        }
-    }
-    Ok(session)
-}
 
 /*
 Blocks
