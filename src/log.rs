e1731969cf071b2244e6f84cd319e0096abe4006
Sl1mj1m14 <99777514+Sl1mj1m14@users.noreply.github.com>
2026-03-21 13:51:00 -0500

    Implemented broken session system

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,31 @@
+//use crate::SESSION
+
+const LOG_DIR: &str = "logs";
+
+use std::{io::Error, path::{Path, PathBuf}};
+
 pub fn log (msg: String) {
     //Handle errors
     //Write to log file
     println!("{msg}");
 }
+
+pub fn close (session: u16, timestamp: String) -> Result<(),Error> {
+
+    let mut log_name: String = String::from("log");
+    if session != 0 {log_name += &session.to_string()}
+    log_name += ".txt";
+    let path: PathBuf = [LOG_DIR,&log_name].iter().collect();
+    println!("Original Path is: {}",path.display());
+    if !path.exists() {return Ok(())}
+    
+    let mut append: i32 = 0;
+
+    let mut rename: String = String::from("log_");
+    rename += &timestamp;
+    let mut rename_path: PathBuf = [LOG_DIR,&rename,".txt"].iter().collect();
+
+    Ok(())
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 //use mc_classic;
 //use mc_classic_js;
 
-use std::path::PathBuf;
+use std::{fs::{File, create_dir_all, read_dir, remove_dir_all, remove_file}, io::{Error, Read, Write}, mem, path::PathBuf};
+use regex::{self, Regex};
+use chrono::prelude::*;
 
 mod read;
 mod write;
@@ -11,9 +13,39 @@ mod functions;
 mod world;
 mod log;
 
-fn main () {
+const SESSION_DIR: &str = "session";
+
+static mut SESSION: u16 = 0;
+
+fn main () -> Result<(),Error> {
     println!("hello rewrite");
 
+    /***Opening Session File***/
+
+    let mut session: u16 = 0;
+
+    match clean_sessions() {
+        Ok(val) => session = val,
+        Err(err) => eprintln!("Session Error: {err}")
+    }
+
+    unsafe {SESSION = session}
+    
+    let mut lock_name: String = String::from("session");
+    if session != 0 {lock_name += &session.to_string()}
+    lock_name += ".lock";
+
+    let session_path: PathBuf = [SESSION_DIR,&lock_name].iter().collect();
+    let timestamp: String = Local::now().format("%Y%m%d%H%M%S").to_string();
+
+    let mut session_lock = File::create(session_path.clone())?;
+    session_lock.write_all(timestamp.as_bytes())?;
+    session_lock = File::open(session_path)?;
+
+    println!("Session Started");
+
+
+/*
     let level = read::read_classic_file(PathBuf::from("input/level.dat"));
 
     match level {
@@ -28,7 +60,62 @@ fn main () {
         Err(error) => eprintln!("{error}")
     }
 
-    
+    */
+    //mem::drop(session_lock);
+    let _ = clean_sessions();
+    Ok(())
+}
+
+fn clean_sessions () -> Result<u16,Error> {
+    let mut session: u16 = 0;
+    let mut open: Vec<u16> = Vec::new();
+
+    create_dir_all(SESSION_DIR)?;
+
+    let pattern = match Regex::new(r"^session(?<num>\d*)\.lock$") {
+        Ok(val) => val,
+        Err(err) => {
+            //This error should never happen, but if it does only log files will break
+            eprintln!("Regex formatting error: {err}");
+            return Ok(0)
+        }
+    };
+
+    for entry in read_dir(SESSION_DIR)? {
+        let path = entry?.path();
+        if path.is_file() {
+            let file_name = path.file_name().unwrap_or_default().to_str().unwrap_or_default();
+            if pattern.is_match(file_name) {
+                println!("{}",path.display());
+                let id: u16 = match pattern.captures(file_name).unwrap()["num"].parse() {
+                    Ok(val) => val,
+                    Err(_) => 0
+                };
+                match File::open(path.clone()) {
+                    Ok(mut file) => {
+                        let mut timestamp: String = String::default();
+                        file.read_to_string(&mut timestamp).unwrap_or_default();
+                        let _ = log::close(id,timestamp);
+                    },
+                    Err(_) => {
+                        open.push(id);
+                        while open.contains(&session) {session += 1}
+                        println!("It is read as open...");
+                    }
+                }
+            }
+            let _ = remove_file(path);
+        } else {
+            match remove_dir_all(path) {
+                Ok(()) => (),
+                Err(err) => {
+                    println!("Broken directory inside session folder");
+                    eprintln!("Error: {err}");
+                }
+            }
+        }
+    }
+    Ok(session)
 }
 
 /*
