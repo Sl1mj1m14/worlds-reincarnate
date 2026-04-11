7e405ebc249265290795755d95c1ce3d731347e5
Sl1mj1m14 <99777514+Sl1mj1m14@users.noreply.github.com>
2026-04-11 08:25:11 -0500

    Added filter to only handle supported editions

--- src/version.rs
+++ src/version.rs
@@ -9,8 +9,9 @@ pub const JAVASCRIPT_EDITION: &str = "classicjs";
 
 pub const J_PC16: i32 = 40; //pc-161148
 pub const J_C12: i32 = 10110; //c0.0.12a_03
-pub const J_C13: i32 = 10140; //c0.0.13a-launcher
+//pub const J_C13: i32 = 10140; //c0.0.13a-launcher
 pub const J_C13_03: i32 = 10150; //c0.0.13a_03-launcher
+pub const J_C14: i32 = 10160; //C0.0.14a
 pub const J_C29: i32 = 10780; //c0.29_02
 pub const J_C30: i32 = 10810; //c0.30c-launcher
 
@@ -44,7 +45,7 @@ pub fn get () -> Result<Vec<Edition>,Error> {
     let mut edition_id: String = String::default();
 
 
-    for (index,line) in reader.records().enumerate() {
+    for (_, line) in reader.records().enumerate() {
         let clean = line?;
         let identifier = clean.get(0).unwrap();
         let display = clean.get(1).unwrap();
@@ -52,12 +53,14 @@ pub fn get () -> Result<Vec<Edition>,Error> {
 
         match identifier {
             "edition" => {
-                if index != 0 {
+                if is_valid(edition_id.clone(), None) && versions.len() > 0 {
                     editions.push ( Edition {
                         id: edition_id,
                         display: edition_display,
                         versions: versions
                     });
+                } else {
+                    log(1, format!("Current version of the converter does not support any versions for {} - skipping", edition_display.clone()));
                 }
                 edition_display = display.to_string();
                 edition_id = id.to_string();
@@ -72,11 +75,15 @@ pub fn get () -> Result<Vec<Edition>,Error> {
                         continue;
                     }
                 };
-                versions.push(
-                    Samvid { 
-                        id: i, 
-                        display: display.to_string()
-                    });
+                if is_valid(edition_id.clone(), Some(i)) {
+                    versions.push(
+                        Samvid { 
+                            id: i, 
+                            display: display.to_string()
+                        });
+                } else { 
+                    log(1, format!("Current version of the converter does not support {} {} - skipping", edition_display.clone(), display)) 
+                }
             }
             _ => {
                 log(1,format!("Unrecognized identifier found: {}",identifier));
@@ -85,14 +92,29 @@ pub fn get () -> Result<Vec<Edition>,Error> {
         }
     }
 
-    if edition_id != "" {
+    if is_valid(edition_id.clone(), None) {
         editions.push(
             Edition {
                 id: edition_id.clone(),
                 display: edition_display.clone(),
                 versions: versions.clone()
             });
+    } else {
+        log(1, format!("Current version of the converter does not support any versions for {} - skipping", edition_display.clone()));
     }
 
     Ok(editions)
+}
+
+fn is_valid (edition: String, version:Option<i32>) -> bool {
+    match edition.as_str() {
+        JAVA_EDITION => {
+            if version.is_some() {
+                if version.unwrap() < J_C14 {true}
+                else {false}
+            } else {true}
+        },
+        JAVASCRIPT_EDITION => true,
+        _ => false
+    }
 }
